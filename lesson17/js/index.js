b01014a2dcaf0bab60d235390907d0ac3450808d
Vadim <vzhukov@gid.ru>
2026-03-09 15:31:45 +0300

    WIP

--- lesson17/js/index.js
+++ lesson17/js/index.js
@@ -1,6 +1,7 @@
 import HeaderFixed from "./header.js";
 import BurgerMenu from "./burger.js";
 import { productSlider } from "./product-slider.js";
+import { sizes } from './sizes.js';
 
 try {
   const headerFixed = new HeaderFixed({
@@ -28,6 +29,7 @@ try {
   );
 
   productSlider();
+  sizes();
 } catch (error) {
   console.error(error);
 }
