5e86d3b7ced4ba4408c71a81222110e94b1f6cc0
Vadim Zhukov <zhukov.vd.s@gmail.com>
2026-07-23 23:07:19 +0300

    w

--- scss_project/vite.config.js
+++ scss_project/vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import { ViteImageOptimizer } from 'vite-plugin-image-optimizer'
 
 export default defineConfig ({
-  base: './',
+  base: '/aroken-school/',
   root: './src',
   server: {
     port: 3000
